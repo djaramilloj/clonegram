a22729818d43e9815e04bde02403d35486cac86f
juan <djaramilloj1@gmail.com>
2020-05-02 13:31:23 -0500

    connected to postgresSQL

--- clonegram/settings.py
+++ clonegram/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'clonegram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'template1',
+        'USER': 'juandavidjaramillo',
+        'PASSWORD': 'Waypooltec2020',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432', 
     }
 }
 
